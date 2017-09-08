38eacea835aa3ce2c17225b3f05e2228c4d2c617
Natalo4ka <natamysh16@gmail.com>
2017-09-09 00:14:28 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static readonly Test t = new Test();
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("1");
+            StringBuilder sb2 = new StringBuilder("1");
+            Console.WriteLine(sb==sb2);
+            Console.WriteLine(sb.Equals(sb2));
+            t.Name = "erere";
+            //t = new Test();
+            const int[] d = { 1, 2, 3 };
+
+        }
+    }
+    class Test:Some
+    {
+        public string Name;
+    }
+    struct Some
+    {
+
+    }
+
+}
